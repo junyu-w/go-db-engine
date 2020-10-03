bbf07cd0b3dc78770949cbd3e23030c9eabb0a06
Junyu Wang <junyuw@berkeley.edu>
2020-10-03 13:25:13 -0700

    added basic interface methods to memtable

--- memtable.go
+++ memtable.go
@@ -1,19 +1,67 @@
 package dbengine
 
-type bytes []byte
-
 // MemTable - A memtable handles the in-memory operatoins of the DB on data that
 // has not been persisted into the file system
 type MemTable interface {
-	Get(key string) ([]byte, error)
-	GetRange(start, end string) ([]bytes, error)
-	Write(key string, value []byte) error
+	// Get - retrieves the value saved with key
+	Get(key string) []byte
+	// GetRange - retrieves all values from specified key range
+	GetRange(start, end string) [][]byte
+	// Write - write key with value into memtable
+	Write(key string, value []byte)
+	// Delete - delete a record with key
+	Delete(key string)
 	// Wal - returns the write-ahead-log instance for write ops recording
 	Wal() Wal
+	// Serialize - serialize the memtable into bytes that can be stored on filesystem
+	Serialize() []byte
 }
 
 // SkipListMemTable - A memtable implementation using the skip list data structure
 type SkipListMemTable struct {
-	s         *skipList
-	totalSize int64
+	s              *skipList
+	TotalSizeBytes uint64 // total size of key, value data stored
+}
+
+// NewMemTable - create a new memtable instance
+func NewMemTable(threshold uint64) *SkipListMemTable {
+	return &SkipListMemTable{
+		s:              newSkipList(),
+		TotalSizeBytes: 0,
+	}
+}
+
+// Get - retrieves the value saved with key
+func (m *SkipListMemTable) Get(key string) []byte {
+	node := m.s.search(key)
+	if node != nil {
+		return node.value
+	}
+	return nil
+}
+
+// Write - write key with value into memtable
+func (m *SkipListMemTable) Write(key string, value []byte) {
+	m.s.upsert(key, value)
+
+	sizeWritten := len(key) + len(value)
+	m.TotalSizeBytes += uint64(sizeWritten)
+}
+
+// Delete - delete a record with key
+// TODO
+func (m *SkipListMemTable) Delete(key string) {
+	// upon deletion, insert a tombstone record instead of performing actual deletion
+}
+
+// Serialize - serialize the memtable into bytes that can be stored on filesystem
+// TODO
+func (m *SkipListMemTable) Serialize() []byte {
+	return nil
+}
+
+// GetRange - retrieves all values from specified key range
+// TODO
+func (m *SkipListMemTable) GetRange(start, end string) [][]byte {
+	return nil
 }
